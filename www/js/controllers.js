5ed24911e633d5f2a77a27eba190fab76fb450b6
Roland Munsil <roland@munsil.com>
2017-02-25 06:14:33 -0600

    Help

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -126,19 +126,31 @@ angular.module('centauriApp.controllers', [])
 
 .controller('FeedCtrl', function($scope, ResearchService, AccountService, AuthenticationService) {
 
-  $scope.researchIndex = 0;
-  $scope.currentResearch = ResearchService.getResearchData(0);
+  $scope.curPaperID = 0;
 
-  $scope.showNewResearch = function() {
-    $scope.researchIndex = $scope.researchIndex + 1;
-    $scope.currentResearch = ResearchService.getResearchData($scope.researchIndex);
-  };
+  $scope.curPaper = function() {
+    return $scope.paperObjects[$scope.curPaperID];
+  }
+
+  $scope.curID = function() {
+    return $scope.curPaperID;
+  }
+
+  $scope.onIgnore = function() {
+    $scope.curPaperID++;
+    
+  }
+  $scope.onSave = function() {
+    $scope.curPaperID++;
+  }
+
 
   var currentUserID = AuthenticationService.getCurrentUserID();
   $scope.paperObjects = ResearchService.getResearchExamples([]);
   $scope.userPrefTags = AccountService.getExplorerGenreTags(currentUserID);
   $scope.savedPapers = AccountService.getExplorerSavedPaperIDs(currentUserID);
   // $scope.userPrefTags = [];
+
 
 })
 
